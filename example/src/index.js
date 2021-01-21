ee0c20bc0af7b7d35c6660d21c17440bfc4a2263
lz1998 <875543533@qq.com>
2021-01-22 03:10:10 +0800

    example

--- example/src/index.js
+++ example/src/index.js
@@ -1,11 +1,3 @@
-# 使用说明
-
-创建nodejs项目
-
-安装`npm install js-pbbot`
-
-代码
-```javascript
 let {createBotWsServer, EventHandler, Msg} =require('js-pbbot')
 
 EventHandler.handlePrivateMessage = async (bot, event) => {
@@ -14,6 +6,3 @@ EventHandler.handlePrivateMessage = async (bot, event) => {
 }
 
 createBotWsServer(8081)
-```
-
-运行[GMC](https://github.com/protobufbot/Go-Mirai-Client/releases)，登陆机器人QQ
